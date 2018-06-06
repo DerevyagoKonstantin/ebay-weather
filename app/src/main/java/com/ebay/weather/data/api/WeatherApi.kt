7902f78ad8f1e6ede606767692f3aad84b5ff845
Konstantin <jetfirer@gmail.com>
2018-06-07 00:36:14 +0200

    Add search weather by gps

--- app/src/main/java/com/ebay/weather/data/api/WeatherApi.kt
+++ app/src/main/java/com/ebay/weather/data/api/WeatherApi.kt
@@ -11,8 +11,21 @@ const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
 interface WeatherApi {
 
     @GET("weather")
-    fun getWeatherByCity(@Query("q") city: String, @Query("appid") apiKey: String): Call<WeatherInfo>
+    fun getWeatherByCity(
+            @Query("q") city: String,
+            @Query("appid") apiKey: String
+    ): Call<WeatherInfo>
 
     @GET("weather")
-    fun getWeatherByZipcode(@Query("zip") zip: String, @Query("appid") apiKey: String): Call<WeatherInfo>
+    fun getWeatherByZipcode(
+            @Query("zip") zip: String,
+            @Query("appid") apiKey: String
+    ): Call<WeatherInfo>
+
+    @GET("weather")
+    fun getWeatherByGps(
+            @Query("lat") lat: String,
+            @Query("lon") lon: String,
+            @Query("appid") apiKey: String
+    ): Call<WeatherInfo>
 }
--- app/src/main/java/com/ebay/weather/data/api/WeatherNetwork.kt
+++ app/src/main/java/com/ebay/weather/data/api/WeatherNetwork.kt
@@ -5,16 +5,16 @@ import java.util.regex.Pattern
 
 
 const val WEATHER_API_KEY = "95d190a434083879a6398aafd54d9e73"
+const val ZIPCODE_REGEX = "^\\d{5}(-\\d{4})?\$"
+const val GPS_REGEX = "^[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?)\\s*,\\s*[-+]?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)\$"
 
 class WeatherNetwork(private val api: WeatherApi) : WeatherRemoteSource {
 
-    private val zipcodeRegex = "^\\d{5}(-\\d{4})?$"
-
     override fun getWeather(search: String): LiveDataCallback<WeatherInfo> {
-        return if (Pattern.matches(zipcodeRegex, search)) {
-            getWeatherByZipcode(search)
-        } else {
-            getWeatherByCity(search)
+        return when {
+            Pattern.matches(ZIPCODE_REGEX, search) -> getWeatherByZipcode(search)
+            Pattern.matches(GPS_REGEX, search) -> getWeatherByGps(search)
+            else -> getWeatherByCity(search)
         }
     }
 
@@ -25,6 +25,13 @@ class WeatherNetwork(private val api: WeatherApi) : WeatherRemoteSource {
 
     private fun getWeatherByCity(city: String): LiveDataCallback<WeatherInfo> {
         val call = api.getWeatherByCity(city, WEATHER_API_KEY)
+        return LiveDataCallback(call)
+    }
+
+    private fun getWeatherByGps(gps: String): LiveDataCallback<WeatherInfo> {
+        val lat = gps.substringBefore(",").trim()
+        val lon = gps.substringAfter(",").trim()
+        val call = api.getWeatherByGps(lat, lon, WEATHER_API_KEY)
         return LiveDataCallback(call)
     }
 }
